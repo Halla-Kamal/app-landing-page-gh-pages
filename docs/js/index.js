fc8f8aeb879d02423bec7e1341830174483e733d
Halla_Sidan <h.saidan96@gmail.com>
2021-06-12 22:53:28 +0300

    first commit

---
+++ docs/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  // valivation form
+  $(function () {
+    $("#emailForm").validate();
+  });
+
+  // WOW animate
+  var wow = new WOW({
+    mobile: false
+  });
+  wow.init();
+});
